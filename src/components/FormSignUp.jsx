c690d1156da3abd879b1f797ed15db270e8e4ef7
luiz <luiz.santana1@aluno.ifsp.edu.br>
2026-03-17 17:50:13 -0300

    fetch

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,66 +1,86 @@
-import { View, Text, StyleSheet, Button } from 'react-native'
-import { TextInput } from 'react-native-web'
-import { useState } from 'react'
+import { View, Text, StyleSheet, Button } from "react-native";
+import { TextInput } from "react-native-web";
+import { useState } from "react";
 
 export default function FormSignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [avatar, setAvatar] = useState("");
 
-  const [nome, setNome] = useState("")
-  const [email, setEmail] = useState("")
-  const [senha, setSenha] = useState("")
-
-  const handleSubmit = () => {
-    console.log({nome, vemail, senha})
-  }
+  const handleSubmit = async () => {
+    console.log({ name, email, pass, avatar });
+    const response = await fetch("http://localhost:3000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, pass, avatar }),
+    });
+    if (response.ok) {
+      console.log("Usuário cadastrado com sucesso");
+      const data = await response.json();
+      console.log(data);
+    } else {
+      console.log("Erro ao criar usuario");
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Cadastro</Text>
       <View style={styles.form}>
-        <TextInput 
-            style={styles.inputs} 
-            placeholder="Nome"
-            value={nome}
-            onChangeText={setNome}
+        <TextInput
+          style={styles.inputs}
+          placeholder="Nome"
+          value={name}
+          onChangeText={setName}
         />
-        <TextInput 
-            style={styles.inputs} 
-            placeholder="Email"
-            value={email}
-            onChangeText={setEmail}
+        <TextInput
+          style={styles.inputs}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
         />
-        <TextInput 
-            style={styles.inputs} 
-            placeholder="Senha" 
-            secureTextEntry
-            value={senha}
-            onChangeText={setSenha}
+        <TextInput
+          style={styles.inputs}
+          placeholder="Senha"
+          secureTextEntry
+          value={pass}
+          onChangeText={setPass}
+        />
+        <TextInput
+          style={styles.inputs}
+          placeholder="Avatar"
+          value={avatar}
+          onChangeText={setAvatar}
         />
         <Button title="Cadastrar" onPress={handleSubmit} />
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f7f7f7',
-    alignItems: 'center'
+    backgroundColor: "#f7f7f7",
+    alignItems: "center",
   },
-  form:{
+  form: {
     width: "90%",
-    gap: 10
+    gap: 10,
   },
-  inputs:{
+  inputs: {
     borderWidth: 1,
     borderColor: "#d1d1d1",
     borderStyle: "solid",
     borderRadius: 6,
-    padding: 8
+    padding: 8,
   },
-    title: {
+  title: {
     fontSize: 18,
     fontWeight: "700",
-    marginBottom: 20
-  }
-})
+    marginBottom: 20,
+  },
+});
